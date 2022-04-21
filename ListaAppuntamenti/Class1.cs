2880fa37bac01877c6bc474cd9201914635c36ae
elvecho <100934190+elvecho@users.noreply.github.com>
2022-04-21 16:04:02 +0200

    commit

---
+++ ListaAppuntamenti/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento
+    {
+        public DateTime DataeOra;
+        public string nome;
+        public string località;
+
+        public Appuntamento(DateTime DataeOra, string nome, string località)
+        {
+            this.DataeOra = DataeOra;
+            this.nome = nome;
+            this.località = località;
+        }
+        public virtual void NomeEsteso()
+        {
+            Console.WriteLine("l appuntamento: all ora: " + DataeOra + "nome: " + nome + " località: " + località);
+        }
+        public override string ToString()
+        {
+            string rappresentazioneInStringa = "";
+
+            rappresentazioneInStringa += "---- appuntamento ---- \n";
+            rappresentazioneInStringa += "Iva: " + this.DataeOra + "\n";
+            rappresentazioneInStringa += "Nome: " + this.nome + "\n";
+            rappresentazioneInStringa += "Descrizione: " + this.località + "\n";
+           
+           
+
+            return rappresentazioneInStringa;
+        }
+        public void Cambiadata(DateTime Nuovadata)
+        {
+            DataeOra = Nuovadata;
+            Console.WriteLine(DataeOra);
+          
+  
+        }
+    }
+}
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ListaAppuntamenti;
 Console.WriteLine("Hello, World!");
+List<Appuntamento> listaDiAppuntamento = new List<Appuntamento>();
+Console.WriteLine("quanti appuntamenti vuoi aggiungere?");
+ int appuntamenti = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < appuntamenti; i++)
+{
+
+    Console.Write("Inserisci una data: ");
+    string dataInserita = Console.ReadLine();
+
+    DateTime dataUtente = DateTime.Parse(dataInserita);
+    Console.Write("Inserisci il nome ");
+    string nomeA = Console.ReadLine();
+
+    Console.Write("Inserisci una località ");
+    string località = Console.ReadLine();
+
+    Appuntamento appuntamento = new Appuntamento(dataUtente, nomeA, località);
+    listaDiAppuntamento.Add(appuntamento);
+
+
+  appuntamento.NomeEsteso();
+    Console.WriteLine("inserisci una nuova data");
+
+    DateTime Nuovadata = DateTime.Parse(Console.ReadLine());
+    appuntamento.Cambiadata(Nuovadata);
+
+}
+foreach (Appuntamento prodottoNellaLista in listaDiAppuntamento)
+{
+    Console.WriteLine(prodottoNellaLista.ToString());
+}
+
+
+
+
+
+
+
